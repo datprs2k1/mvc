8d193f4004ec5a5b6ba5e064ce3264b1dd2b0454
datprs <datprs@gmail.com>
2022-02-27 17:13:47 +0700

    Fix lai routes

--- core/routes.php
+++ core/routes.php
@@ -1,8 +1,45 @@
 <?php
 
+
 $controllers = [
     'Home' => ['index', 'about'],
 ];
+
+$routes = [
+    'about' => [
+        'controller' => 'Home',
+        'action' => 'about'
+    ],
+];
+
+$url = isset($_GET['url']) ? $_GET['url'] : '';
+$url = trim($url, '/');
+$url = explode('/', $url);
+
+
+if (array_key_exists($url[0], $routes)) {
+    $route = $routes[$url[0]];
+    $controller = ucfirst($route['controller']);
+    $action = $route['action'];
+    if (!empty($url[1])) {
+        $param = $url[1];
+    }
+} else {
+    if (!empty($url[0])) {
+        $controller = ucfirst($url[0]);
+        if (!empty($url[1])) {
+            $action = $url[1];
+            if (!empty($url[2])) {
+                $param = $url[2];
+            }
+        } else {
+            $action = 'index';
+        }
+    } else {
+        $controller = 'Home';
+        $action = 'index';
+    }
+}
 
 if (!array_key_exists($controller, $controllers) || !in_array($action, $controllers[$controller])) {
     $controller = 'Home';
@@ -15,7 +52,7 @@ $controller_name = $controller . 'Controller';
 
 $controller = new $controller_name();
 
-if (isset($id)) {
+if (isset($param)) {
     $controller->{$action}($param);
 } else {
     $controller->{$action}();
--- index.php
+++ index.php
@@ -1,25 +1,6 @@
 <?php
 
 
-$url = isset($_GET['url']) ? $_GET['url'] : '';
-$url = rtrim($url, '/');
-$url = explode('/', $url);
-
-if (!empty($url[0])) {
-    $controller = ucfirst($url[0]);
-    if (!empty($url[1])) {
-        $action = $url[1];
-        if (!empty($url[2])) {
-            $param = $url[2];
-        }
-    } else {
-        $action = 'index';
-    }
-} else {
-    $controller = 'Home';
-    $action = 'index';
-}
-
 require_once('core/database.php');
 
 require_once('models/Model.php');
@@ -29,5 +10,3 @@ require_once('core/request.php');
 require_once('controllers/BaseController.php');
 
 require_once('core/routes.php');
-
-$request = new Request();
