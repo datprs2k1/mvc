41e429cab7db9ced93925fd5a18a05b9e98e1c16
datprs <datprs@gmail.com>
2022-03-06 18:33:02 +0700

    Them view_admin

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -19,6 +19,21 @@ class BaseController
         }
     }
 
+    public function view_admin($file, $data = [])
+    {
+        $view_file = $this->view_folder . '/' . str_replace('.', '/', $file) . '.php';
+
+        if (file_exists($view_file)) {
+            extract($data);
+            ob_start();
+            require_once $view_file;
+            $content = ob_get_clean();
+            require_once 'views/layouts/applicationAdmin.php';
+        } else {
+            echo 'View file does not exist!';
+        }
+    }
+
     public function model($model)
     {
         $model_file = 'models/' . $model . '.php';
